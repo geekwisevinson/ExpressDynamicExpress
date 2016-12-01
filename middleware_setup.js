d07943c6204bf0c001224eb54a930d29ff0b62db
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2016-12-01 15:45:59 -0800

    dynamics

---
+++ middleware_setup.js
@@ -0,0 +1,18 @@
+
+
+module.exports=()=> {
+	app.use(logger('dev'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(cookieParser());
+	console.log('middleware setup');
+
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header('Access-Control-Allow-Methods','POST, GET, DELETE, PUT');
+		next();
+	});
+};
+
+
