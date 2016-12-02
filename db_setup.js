e64026496b0bad8b1c06cd870747131b9bf82646
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2016-12-01 16:00:59 -0800

    dynamic

--- db_setup.js
+++
@@ -1,7 +0,0 @@
-module.exports =  () =>{
-	mongoose.connect('mongodb://branchadmin:badmin@ds135797.mlab.com:35797/branchesdb');
-	mongoose.Promise = Promise;
-	const db = mongoose.connection;
-	db.on('open', () => console.log('Database connected'));
-	db.on('error', (err) => console.log(`DB ERROR: ${err}`));
-};
